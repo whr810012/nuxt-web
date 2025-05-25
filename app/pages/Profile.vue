d87fb8019da3008f49fcf05b345b812516ff916c
whr810012 <102893406@qq,com>
2025-05-25 22:51:28 +0800

    后置处理

--- app/pages/Profile.vue
+++ app/pages/Profile.vue
@@ -177,45 +177,49 @@ const errors = reactive({
   password: '',
   sex: ''
 });
-
-// 初始化表单数据
-onMounted(async() => {
-  await nextTick()
-  const init = async () => {
-    const token = localStorage.getItem('token')
-    const userId = localStorage.getItem('userId')
-    if (!!token) {
-      console.log('token11', token)
-      userStore.setToken(token)
-      try {
-        console.log('开始请求用户信息')
-        let userInfo = await get(`/user/query`)
-        if (userInfo.email) {
-          userInfo.email = decrypt(userInfo.email)
-        }
-        if (userInfo.phonenumber) {
-          userInfo.phonenumber = decrypt(userInfo.phonenumber)
-        }
-        console.log('userInfo 响应数据:', userInfo);
-        userStore.setUserInfo(userInfo)
-        console.log('存储后的用户信息:', userStore.getUserInfo);
-      } catch (error) {
-        console.log('获取用户信息失败', error);
-        // 打印更详细的错误信息
-        if (error.response) {
-          // 服务器响应了，但状态码不是2xx
-          console.log('错误状态:', error.response.status);
-          console.log('错误数据:', error.response.data);
-        } else if (error.request) {
-          // 请求发出但没有收到响应
-          console.log('没有收到响应:', error.request);
-        } else {
-          // 设置请求时发生错误
-          console.log('请求错误:', error.message);
-        }
+const init = async () => {
+  const token = localStorage.getItem('token')
+  if (!!token) {
+    console.log('token11', token)
+    userStore.setToken(token)
+    try {
+      console.log('开始请求用户信息')
+      let userInfo = await get(`/user/query`)
+      if (userInfo.email) {
+        userInfo.email = decrypt(userInfo.email)
+      }
+      if (userInfo.phonenumber) {
+        userInfo.phonenumber = decrypt(userInfo.phonenumber)
+      }
+      console.log('userInfo 响应数据:', userInfo);
+      userStore.setUserInfo(userInfo)
+      if (userInfo.value) {
+        profileData.nickName = userInfo.value.nickName || ''
+        profileData.userName = userInfo.value.userName || ''
+        profileData.email = userInfo.value.email || ''
+        profileData.phonenumber = userInfo.value.phonenumber || ''
+        profileData.sex = userInfo.value.sex
+      }
+    } catch (error) {
+      console.log('获取用户信息失败', error);
+      // 打印更详细的错误信息
+      if (error.response) {
+        // 服务器响应了，但状态码不是2xx
+        console.log('错误状态:', error.response.status);
+        console.log('错误数据:', error.response.data);
+      } else if (error.request) {
+        // 请求发出但没有收到响应
+        console.log('没有收到响应:', error.request);
+      } else {
+        // 设置请求时发生错误
+        console.log('请求错误:', error.message);
       }
     }
   }
+}
+// 初始化表单数据
+onMounted(async () => {
+  await nextTick()
   await init()
   if (userInfo.value) {
     profileData.nickName = userInfo.value.nickName || ''
@@ -306,13 +310,7 @@ const updateProfile = async () => {
     changeForm.append('id', userStore.getUserInfo.id)
     // 调用API更新用户信息
     const response = await post('/user/amend', changeForm)
-
-    // if (response.code === 200) {
-    //   // 更新本地存储的用户信息
-    //   userStore.setUserInfo({
-    //     ...userInfo.value,
-    //     ...profileData
-    //   })
+    init()
     showToast('个人资料更新成功')
     // }
   } catch (error) {
